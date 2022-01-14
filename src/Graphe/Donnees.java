54c8d03ec3385045970d4175ea7dea8668102441
diakarou sokhona <diaksokhona1993@mail.com>
2022-01-15 00:38:11 +0100

    visualisation dynamique

--- src/Graphe/Donnees.java
+++ src/Graphe/Donnees.java
@@ -1,134 +1,86 @@
 package Graphe;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-
-import org.graphstream.graph.Edge;
-import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
-import org.graphstream.graph.implementations.DefaultGraph;
 
 public class Donnees {
-  private final static String FILE_NAME = "Data/soc-sign-bitcoinalpha.csv";
 
-    private ArrayList<Donnees> tabDonnees;
-    private String source;
-    private String target; 
-    private String rating;
-    private Integer time;
-    
-    
-    public Donnees() {
-      tabDonnees =new  ArrayList<Donnees>();
-      source=null;
-      target=null;
-      rating=null;
-      time=0;
-    }
-
-    public String getSource() {
-      return source;
-    }
-
-    public void setSource(String source) {
-      this.source = source;
-    }
-
-    public String getTarget() {
-      return target;
-    }
-
-    public void setTarget(String target) {
-      this.target = target;
-    }
-
-    public String getRating() {
-      return rating;
-    }
-
-    public void setRating(String rating) {
-      this.rating = rating;
-    }
-
-    public Integer getTime() {
-      return time;
-    }
-
-    public void setTime(Integer time) {
-      this.time = time;
-    }
-    
-    public static String getResourcePath(String fileName) {
-      final File f = new File("");
-      final String dossierPath = f.getAbsolutePath() + File.separator + fileName;
-      return dossierPath;
-    }
-
-    public static File getResource(String fileName) {
-      final String completeFileName = getResourcePath(fileName);
-      File file = new File(completeFileName);
-      return file;
-    }
-    public void init(List<String> line) throws IOException {
-        Donnees donnees=new Donnees();
-        donnees.setSource(line.get(0));
-        donnees.setTarget(line.get(1));
-        donnees.setRating(line.get(2));
-        donnees.setTime(Integer.parseInt(line.get(3)));
-        tabDonnees.add(donnees);
-    }
+  private ArrayList<Donnees> tabDonnees;
+  private String source;
+  private String target; 
+  private String rating;
+  private Integer time;
 
 
-    public ArrayList<Donnees> getTabDonnees() {
-      return tabDonnees;
-    }
+  public Donnees() {
+    tabDonnees =new  ArrayList<Donnees>();
+    source=null;
+    target=null;
+    rating=null;
+    time=0;
+  }
 
-    public void setTabDonnees(ArrayList<Donnees> tabDonnees) {
-      this.tabDonnees = tabDonnees;
-    }
+  public String getSource() {
+    return source;
+  }
 
-    public static void main(String[] args) {
-//      Donnees donnees=new Donnees();
-//      File f=getResource(FILE_NAME);
-//
-//      try {
-//        //donnees.init(f);
-//      } catch (IOException e) {
-//        // TODO Auto-generated catch block
-//        e.printStackTrace();
-//      }
-//         Graph  GG=new DefaultGraph("graph");
-//
-//      int k=0;
-//      Node v=null;
-//      Node w=null;
-//      ArrayList<String> tab=new ArrayList<String>();
-//      for (Donnees string : donnees.getTabDonnees()) {
-//        if(!tab.contains(string.source)) {
-//          tab.add(string.source);
-//        }
-//        if(!tab.contains(string.target)) {
-//          tab.add(string.target);
-//        }
+  public void setSource(String source) {
+    this.source = source;
+  }
 
-//        if (GG.getNode(string.source)==null) {
-//          v = GG.addNode(string.source);
-//        }
-//        if (GG.getNode(string.target)==null) {
-//           w= GG.addNode(string.target);
-//        }
-//        Edge e = GG.addEdge(v.getId()+"-->"+w.getId(),v.getId(),w.getId(),true);
-      }
-     // System.out.println(tab.size());
-//      for (String string : tab) {
-//        System.out.println(string);
-//
-//      }
+  public String getTarget() {
+    return target;
+  }
 
-    //}
-    
+  public void setTarget(String target) {
+    this.target = target;
+  }
+
+  public String getRating() {
+    return rating;
+  }
+
+  public void setRating(String rating) {
+    this.rating = rating;
+  }
+
+  public Integer getTime() {
+    return time;
+  }
+
+  public void setTime(Integer time) {
+    this.time = time;
+  }
+
+  public static String getResourcePath(String fileName) {
+    final File f = new File("");
+    final String dossierPath = f.getAbsolutePath() + File.separator + fileName;
+    return dossierPath;
+  }
+
+  public static File getResource(String fileName) {
+    final String completeFileName = getResourcePath(fileName);
+    File file = new File(completeFileName);
+    return file;
+  }
+  public void init(String source,String target,String rating,String time) throws IOException {
+    Donnees donnees=new Donnees();
+    donnees.setSource(source);
+    donnees.setTarget(target);
+    donnees.setRating(rating);
+    donnees.setTime(Integer.parseInt(time));
+    tabDonnees.add(donnees);
+  }
+
+
+  public ArrayList<Donnees> getTabDonnees() {
+    return tabDonnees;
+  }
+
+  public void setTabDonnees(ArrayList<Donnees> tabDonnees) {
+    this.tabDonnees = tabDonnees;
+  }
+
+
 }
--- src/Graphe/gerationGraphe.java
+++ src/Graphe/gerationGraphe.java
@@ -5,27 +5,24 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.DefaultGraph;
-import org.graphstream.stream.file.FileSource;
-import org.graphstream.stream.file.FileSourceEdge;
 
 
 public class gerationGraphe {
-  
+
   private final static String FILE_NAME_CSV = "Data/soc-sign-bitcoinalpha.csv";
   private final static String FILE_NAME_TXT = "Data/soc-sign-bitcoinalpha.txt";
-  
- private  static Graph graphe;
- private static List<String> data;
- 
+
+  private  static Graph graphe;
+  private static List<String> data;
+  private static Donnees donnees;
+
   public   gerationGraphe(Graph graph) {}
-  
+
   public static String getResourcePath(String fileName) {
     final File f = new File("");
     final String dossierPath = f.getAbsolutePath() + File.separator + fileName;
@@ -76,7 +73,7 @@ public class gerationGraphe {
     bufferedWriter.close();
 
   }
-  
+
   public static void visualisationStatique(List<String> tab,Graph graphe) throws Exception  {
     graphe=new DefaultGraph("graphe");
     for (int i = 0; i < tab.size(); i++) {
@@ -87,11 +84,11 @@ public class gerationGraphe {
 
       }
       if(graphe.getNode(ligne[0])==null) {
-       graphe.addNode(ligne[0]);
+        graphe.addNode(ligne[0]);
 
       }
     }
-    
+
     for (int i = 0; i < tab.size(); i++) {
       String b=tab.get(i);
       String[] ligne = b.split(",");
@@ -99,28 +96,96 @@ public class gerationGraphe {
         graphe.addEdge(ligne[0]+"-->"+ligne[1],ligne[0],ligne[1],true);
       }
     }
-    System.out.println("nombre noeud**** "+graphe.getNodeCount());;
-
-    System.out.println("Nombre de noeud++:"+graphe.getNodeCount());
+    System.out.println("Nombre de noeud:"+graphe.getNodeCount());
     System.out.println("Nombre de arret:"+graphe.getEdgeCount());
     System.setProperty("org.graphstream.ui", "swing");
-    graphe.display();
+    //graphe.display();
 
   }
-  
-  
+
+  public static void visualisationDynamique(List<String> tab,Graph graphe,int taille) throws Exception  {
+    int compter=0;
+    //ajout des noeuds
+    donnees=new Donnees();
+    graphe=new DefaultGraph("graphe");
+    for (int i = 0; i < tab.size(); i++) {
+      String b=tab.get(i);
+      String[] ligne = b.split(",");
+      if(graphe.getNode(ligne[1])==null) {
+        graphe.addNode(ligne[1]);
+
+      }
+      if(graphe.getNode(ligne[0])==null) {
+        graphe.addNode(ligne[0]);
+
+      }
+    }
+    //ajout des sommets
+    for (int i = 0; i < tab.size(); i++) {
+      String b=tab.get(i);
+      String[] ligne = b.split(",");
+      if(graphe.getEdge(ligne[0]+"-->"+ligne[1])==null) {
+        graphe.addEdge(ligne[0]+"-->"+ligne[1],ligne[0],ligne[1],true);
+        donnees.init(ligne[0], ligne[1], ligne[2], ligne[3]);
+      }
+    }
+    System.out.println(donnees.getTabDonnees().size());;
+    Graph graphe1=new DefaultGraph("graphe1");
+    System.setProperty("org.graphstream.ui", "swing");
+
+    graphe1.display();
+    int debut=1289192400;
+    int fin=1453438800;
+    while(debut<=fin) {
+      for (int i = 0; i < donnees.getTabDonnees().size(); i++) {
+        if (donnees.getTabDonnees().get(i).getTime()>=debut && donnees.getTabDonnees().get(i).getTime()<=debut+taille ) {//intervalle temps
+          if(graphe1.getNode(donnees.getTabDonnees().get(i).getSource())==null) {
+            graphe1.addNode(donnees.getTabDonnees().get(i).getSource());
+
+          }
+          if(graphe1.getNode(donnees.getTabDonnees().get(i).getTarget())==null) {
+            graphe1.addNode(donnees.getTabDonnees().get(i).getTarget());
+
+          }
+          //ajout des sommets
+          if(graphe1.getEdge(donnees.getTabDonnees().get(i).getSource()+"-->"+donnees.getTabDonnees().get(i).getTarget())==null) {
+            graphe1.addEdge(donnees.getTabDonnees().get(i).getSource()+"-->"+donnees.getTabDonnees().get(i).getTarget(),donnees.getTabDonnees().get(i).getSource(),donnees.getTabDonnees().get(i).getTarget(),true);
+          }
+        }
+
+
+
+      }
+
+
+      Thread.sleep( 1500);
+      System.out.println("Nombre de noeud:"+graphe1.getNodeCount());
+      compter+=graphe1.getNodeCount();
+      graphe1.clear();
+      debut+=taille;
+
+    }
+    System.out.println("compter:"+compter);
+
+  }
+
+
+
+
+
+
   @SuppressWarnings("unused")
   private static void testTransformationCsvEnTxt() throws Exception {
     File f=getResource(FILE_NAME_CSV);
-     data = readFile(f);
+    data = readFile(f);
     transformationCsvEnTx(data,FILE_NAME_TXT);
 
   }
 
   public static void main(String[] args) throws Exception   {
     testTransformationCsvEnTxt();
-
-    visualisationStatique(data, graphe);
+    //visualisationStatique(data, graphe);
+    visualisationDynamique(data, graphe,500000);
 
 
   }
